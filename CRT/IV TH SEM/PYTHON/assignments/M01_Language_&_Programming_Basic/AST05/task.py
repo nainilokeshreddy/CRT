aa23601467b5967bbe56e8c2a5ab7df114fb1e00
nainilokeshreddy <nainilokeshreddy1509@gmail.com>
2026-02-08 16:47:22 +0530

    Implement Collatz Sequence function

--- CRT/IV TH SEM/PYTHON/assignments/M01_Language_&_Programming_Basic/AST05/task.py
+++ CRT/IV TH SEM/PYTHON/assignments/M01_Language_&_Programming_Basic/AST05/task.py
@@ -2,7 +2,11 @@ from typing import List
 
 
 def Collatz_Sequence(n: int)-> List:
-   pass
+   if n % 2 == 0:
+       return [n] + Collatz_Sequence(n//2)
+   elif n % 2 == 1 and n != 1:
+       return [n] + Collatz_Sequence(3*n + 1)
+   else:       return [n]
 if __name__ == '__main__':
     n = int(input())
     print(Collatz_Sequence(n))
